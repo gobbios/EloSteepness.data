5aecdbc394febc717dbec17e9d0f91d8b592a86c
Christof Neumann <christofneumann1@gmail.com>
2022-06-15 11:22:39 +0200

    update cred_width data

--- R/z_data_cred_width.R
+++ R/z_data_cred_width.R
@@ -1,7 +1,7 @@
-#' raw data for analysis of credible interval width as function of observation effort
+#' raw data for analysis of credible interval width as function of interaction ratios
 #'
 #'
-#' @format \code{cred_width} is a data frame with 500 observations, each line
+#' @format \code{cred_width} is a data frame with 1,670 observations, each line
 #'         corresponding to one matrix with steepness evaluated and extracted
 #'         width of the 89\% credible interval
 "cred_width"
